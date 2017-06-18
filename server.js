2d3fd96b3fc5b659ccae114b1f503cdc7a7f43cd
Ayaskanta Panigrahi <ayaskanta.panigrahi@gmail.com>
2017-06-18 20:10:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,11 +79,13 @@ function createTemplate(data){
         `;
         return htmlTemplate; 
 }
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+
+
 
 
 app.get('/:articleName', function(req, res){
