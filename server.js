2d99676dbd5d0e2e679dd1ce10729311c9fcb1ee
Ayaskanta Panigrahi <ayaskanta.panigrahi@gmail.com>
2017-06-18 20:12:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,114 +1,36 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-var articles={
-
-          articleOne:{
-             title: 'Article-one-Ayas',
-             heading:'Article one',
-             date:'Jun 18 2017',
-             content:`<p>
-                     this is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html file
-                 </p>
-                  <p>
-                     this is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html file
-                 </p>`
-          
-                  },
-          articleTwo:{
-              title: 'Article-two-Ayas',
-              heading:'Article two',
-              date:'Jun 15 2017',
-              content:`<p>
-                     this is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html filethis is my first html file
-                 
-                 </p>`},
-          articleThree:{
-              title: 'Article-three-Ayas',
-              heading:'Article three',
-              date:'Jun 14 2017',
-              content:`<p>
-                     this is my third html filethis 
-                 </p>`},
-};
-
-function createTemplate(data){
-    
-        var title = data.title;
-        var heading =  data.heading;
-        var date = data.date;
-        var content = data.content;
-            
-        
-        var htmlTemplate=`
-            <html>
-            <head>
-             <title>
-             ${title}
-             </title>
-             <meta name="viewport" content="width-device-width, initial-scale-1"/>
-             <link href="/ui/style.css" rel="stylesheet" />
-            
-            </head>
-            <body>
-            <div class="container">
-                
-            
-              <div>
-                 <a href="/">home</a>
-                </div>
-                <hr/>
-              <h3>${heading}</h3>
-              <div>
-                 ${date}
-              </div>
-              <div>
-             ${content}
-               
-              </div>
-             </div>
-             
-             
-            </body>
-            </html>
-        
-        `;
-        return htmlTemplate; 
-}
-
-
-
-
-
-
-app.get('/:articleName', function(req, res){
-    res.send(createTemplate(articleOne));
-});
-app.get('/article-two', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui','article-two.html'));
-});
-app.get('/article-three', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui','article-three.html'));
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+    <html>
+    <head>
+     <title>
+     Article-Two-Ayas
+     </title>
+     <meta name="viewport" content="width-device-width, initial-scale-1"/>
+     <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+    <div class="container">
+     <div>
+         <a href="/">home</a>
+     </div>
+     <hr/>
+     <h3>Article Two</h3>
+     <div>
+         Jun 18 2017
+     </div>
+     <div>
+         <p>
+             this is my second html filethis is my second html filethis is my second html filethis is my second html filethis is my second html filethis is my first html filethis is my first html filethis is my first html file
+         </p>
+          <p>
+             this is my second html filethis is my second html filethis is my second html filethis is my second html filethis is my second html filethis is my first html filethis is my first html filethis is my first html file
+         </p>
+       
+         
+         
+     </div>
+     </div>
+     
+     
+    </body>
+    </html>
+ 
+   
